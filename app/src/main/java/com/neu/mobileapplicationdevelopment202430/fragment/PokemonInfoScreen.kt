c76e63564a4f038962bd274193b6b8df9b071f5e
gaikwadsid <sidgaikwad853@gmail.com>
2025-04-08 22:10:21 -0400

    Type Text Color based on type

--- app/src/main/java/com/neu/mobileapplicationdevelopment202430/fragment/PokemonInfoScreen.kt
+++ app/src/main/java/com/neu/mobileapplicationdevelopment202430/fragment/PokemonInfoScreen.kt
@@ -102,7 +102,7 @@ fun PokemonInfoScreen(pokemon: PokemonItem, navController: NavController) {
                     text = pokemon.type1,
                     fontSize = 24.sp,
                     fontWeight = FontWeight.Normal,
-                    color = Color.Black
+                    color = getTypeColor(pokemon.type1)
                 )
 
 
@@ -111,7 +111,7 @@ fun PokemonInfoScreen(pokemon: PokemonItem, navController: NavController) {
                         text = pokemon.type2,
                         fontSize = 24.sp,
                         fontWeight = FontWeight.Normal,
-                        color = Color.Black
+                        color = getTypeColor(pokemon.type2)
                     )
                 }
 
@@ -166,3 +166,24 @@ fun PokemonInfoScreen(pokemon: PokemonItem, navController: NavController) {
 }
 
 
+val typeColors = mapOf(
+    "normal" to Color(0xFFA8A878),
+    "fire" to Color(0xFFF08030),
+    "water" to Color(0xFF6890F0),
+    "electric" to Color(0xFFF8D030),
+    "grass" to Color(0xFF78C850),
+    "ice" to Color(0xFF98D8D8),
+    "fighting" to Color(0xFFC03028),
+    "poison" to Color(0xFFA040A0),
+    "ground" to Color(0xFFE0C068),
+    "flying" to Color(0xFFA890F0),
+    "psychic" to Color(0xFFF85888),
+    "bug" to Color(0xFFA8B820),
+    "rock" to Color(0xFFB8A038),
+    "ghost" to Color(0xFF705898),
+    "dragon" to Color(0xFF7038F8),
+)
+
+fun getTypeColor(type: String): Color {
+    return typeColors[type.lowercase()] ?: Color.Black
+}
